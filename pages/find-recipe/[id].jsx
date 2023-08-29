f184aa366a44f22bba7e648fa365cb1502efd967
minechoo <choominhae@gmail.com>
2023-08-29 10:43:34 +0900

    정규표현식, 배열=객체

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -9,6 +9,12 @@ import { useState, useEffect } from 'react';
 import List from '@/components/atoms/List/List';
 
 function Detail() {
+	// const arr = ['red', 'green', 'blue']; //이 배열은 실상 객체
+	// console.log(arr?.[0]); //arr객체에 0번째 변수에 참조값을 가져옴
+	//정규표현식에서 해단조건이 포함이 아닌 딱 그 조건에 부합될때만 처리 ^조건$
+	//표현식 뒤에 + 는 해당조건의 값이 반복되는 경우에도 true로 인지
+	// const result = /^\d+[.][' ']$/.test('23. ');
+	// console.log(result);
 	const router = useRouter();
 	const { id } = router.query;
 	const { data } = useRecipeById(id);
@@ -31,13 +37,17 @@ function Detail() {
 			}));
 			setTableData(ingredients);
 
-			let intructions = data.strInstructions
-				.split('.')
-				//반환된 문자배열중에서 숫자가 포함되어 있는 배열을 제외
-				//.filter((text) => !/\d/.test(text))
-				.map((text) => text.trim().replace('\r\n', '').trim() + '.')
-				.filter((text) => text !== '.');
-			setListData(intructions);
+			let instructions = data.strInstructions
+				//\r\n이 강제 줄바꿈하는 정규표현식이므로 해당 정규표현식을 구분점으로 문장을 나누는게 효율적
+				.split('\r\n')
+				//분리된 문장중에서 .\t라는 탭띄우기 정규표현식을 제거하기위해서 일단은 공통화할 수 있는 숫자를 제외한 특수기호만 +로 치환
+				//이후 치환된 +기준으로 뒤에값만 map으로 리턴
+				//특정 레시피에는 .\t가 없는 문장도 있기 때문에 해당 구분점이 없을떄는 기존 text를 리턴 그렇지 않으면 치환해서 리턴
+				.map((text) => (text.includes('.\t') ? text.replace('.\t', '+').split('+')[1] : text))
+				//원본 문자열에 줄바꿈 정규표현식이 여러번 들어가 있는 문장의 경우는 빈문장을 배열로 반환하기 때문에 해당 해당 배열값을 제거
+				.filter((text) => text !== '');
+			setListData(instructions);
+			//console.log(instructions);
 		}
 	}, [data]);
 
